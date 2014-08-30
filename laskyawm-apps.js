cc1272391e6393dbc1c69d0c5ad89fc894858987
Daniel Huigens <d.huigens@gmail.com>
2014-08-30 13:48:56 +0200

    Add icons and proper names to app launcher

--- laskyawm-apps.js
+++ laskyawm-apps.js
@@ -23,12 +23,44 @@ loadApps(function(fragment) {
 function loadApps(callback) {
 	var fragment = document.createDocumentFragment();
 	getFile('/Apps/', {codec: 'dir'}, function(contents) {
-		$.each(contents, function(line, attrs) {
+		var total = 0, done = 0, apps = {};
+		Object.keys(contents).forEach(function(line) {
 			if(line.substr(-9) !== '.history/') {
+				total++;
+				getFile('/Apps/' + line + 'manifest.webapp', function(manifest) {
+					manifest = manifest ? JSON.parse(manifest.replace(/^\uFEFF/, '')) : {};
+					var name = manifest.name || line[0].toUpperCase() + line.substr(1, line.length - 2);
+					if(manifest.icons) {
+						console.log(manifest.icons[Object.keys(manifest.icons)[0]]);
+						prepareUrl(manifest.icons[Math.min.apply(Math, Object.keys(manifest.icons))], {relativeParent: '/Apps/' + line, rootParent: '/Apps/' + line}, function(url) {
+							apps[line] = {name: name, path: line, iconUrl: url};
+							maybeCont();
+						});
+					} else {
+						apps[line] = {name: name, path: line, iconUrl: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='}; // Blank gif
+						maybeCont();
+					}
+				});
+			}
+		});
+		function maybeCont() {
+			if(++done === total) cont();
+		}
+		function cont() {
+			console.log(apps);
+			var keys = Object.keys(apps);
+			keys.alphanumSort();
+			console.log(keys);
+			keys.forEach(function(key) {
+				var props = apps[key];
 				var app = document.createElement('div');
 				app.className = 'app';
-				app.textContent = line.substr(0, line.length - 1);
+				app.textContent = props.name;
+				var icon = document.createElement('img');
+				icon.src = props.iconUrl;
+				app.insertBefore(icon, app.firstChild);
 				app.tabIndex = '0';
+				app.title = '/Apps/' + props.path;
 				app.addEventListener('click', click);
 				app.addEventListener('keypress', function(evt) {
 					if(evt.which === 13) {
@@ -37,15 +69,15 @@ function loadApps(callback) {
 					}
 				});
 				function click() {
-					openWindow('/Apps/' + line, {
+					openWindow('/Apps/' + props.path, {
 						originDiv: $('.window.focused')[0],
 						loaderElm: toggleAppsLoader
 					});
 				}
 				fragment.appendChild(app);
-			}
-		});
-		callback(fragment);
+			});
+			callback(fragment);
+		}
 	});
 }
 
