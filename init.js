fc8b4b1223c6355b36cd96a73264896514f3724d
Daniel Huigens <d.huigens@gmail.com>
2015-08-27 21:59:55 +0200

    Prefetch startup.js and laskyawm

--- init.js
+++ init.js
@@ -13,4 +13,17 @@ GET('object/' + sjcl.codec.hex.fromBits(files_hmac.mac('/Core/core.js')), functi
 document.getElementById('container').style.display = 'none';
 var iframe = document.getElementsByTagName('iframe')[0];
 if(iframe) iframe.style.display = 'none';
+
+[
+	'/Core/startup.js',
+	'/settings',
+	'/Core/compat.js',
+	'/Core/laskyawm.html',
+	'/CoreData/laskyawm-localStorage'
+].forEach(function(url) {
+	var link = document.createElement('link');
+	link.rel = 'prefetch';
+	link.href = 'object/' + sjcl.codec.hex.fromBits(files_hmac.mac(url));
+	document.body.appendChild(link);
+});
 //# sourceURL=/Core/init.js
